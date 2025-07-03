c194b8d25e6833e012eabd9f00365d3722b8a025
aayu0394 <chouhanarjun556@gmail.com>
2025-07-03 10:28:25 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,7 @@
+  <script>
+    document.getElementById('contact-form').addEventListener('submit', function(e) {
+      e.preventDefault();
+      alert('Thanks for reaching out! I will get back to you soon.');
+      this.reset();
+    });
+  </script>
